43eaf0d64c31fe0406a65e88b356d9228c571334
blank2017 <33206234+blank2017@users.noreply.github.com>
2022-07-22 14:42:24 +0700

    Add files via upload

---
+++ data_music.js
@@ -0,0 +1,85 @@
+let songs = [
+    {
+        name: 'Over The Galaxy',
+        path:'./Asses/Music/[New Age] Plum - Over The Galaxy.mp3',
+        artist: 'Plum',
+        cover: './Asses/img/over the galaxy.jpg'
+    },
+
+    {
+        name: 'R',
+        path:'./Asses/Music/Plum - R.mp3',
+        artist: 'Plum',
+        cover: './Asses/img/R.jpg'
+    },
+
+    {
+        name: 'Loadstar',
+        path:'./Asses/Music/M2U & Nicode - Loadstar.mp3',
+        artist: 'M2U & Nicode',
+        cover: './Asses/img/Loadstar.jpg'
+    },
+
+    {
+        name: 'Luv Letter',
+        path:'./Asses/Music/Luv Letter - DJ Okawari_ Tsukino Sora_ T.mp3',
+        artist: 'DJ Okawari',
+        cover: './Asses/img/Luv Letter.jpg'
+    },
+
+    {
+        name: 'Lanterns',
+        path:'./Asses/Music/Lanterns - Xomu.mp3',
+        artist: 'Xomu',
+        cover: './Asses/img/Lanterns.jpg'
+    },
+
+    {
+        name: 'Sairai',
+        path:'./Asses/Music/Sairai - Shinchi Kobayashi.mp3',
+        artist: 'Shinchi Kobayashi',
+        cover: './Asses/img/Sairai.jpg'
+    },
+
+    {
+        name: 'Wings Of Piano',
+        path:'./Asses/Music/Wings Of Piano - Various Artists.mp3',
+        artist: 'Various Artists',
+        cover: './Asses/img/Wings Of Piano.jpg'
+    },
+
+    {
+        name: 'The Truth That You Leave',
+        path:'./Asses/Music/The truth that you leave - Pianoboy_.mp3',
+        artist: 'Pianoboy',
+        cover: './Asses/img/The Truth That You Leave.png'
+    },
+
+    {
+        name: 'Somewhere',
+        path:'./Asses/Music/Somewhere - July.mp3',
+        artist: 'July',
+        cover: './Asses/img/Somewhere.jpg'
+    },
+
+    {
+        name: 'Flower Dance',
+        path:'./Asses/Music/Flower Dance - DJ Okawari.mp3',
+        artist: 'DJ Okawari',
+        cover: './Asses/img/Flower Dance.jpg'
+    },
+
+    {
+        name: 'Pneumatic Tokyo',
+        path:'./Asses/Music/Pneumatic Tokyo - EnV.mp3',
+        artist: 'EnV',
+        cover: './Asses/img/EnV.jpg'
+    },
+
+    {
+        name: 'Setsuna',
+        path:'./Asses/Music/Xomu & Justin Klyvis - Setsuna.mp3',
+        artist: 'Xomu & Justin Klyvis',
+        cover: './Asses/img/Setsuna.jpg'
+    },
+]
